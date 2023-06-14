e73ab7b16cc1a5dc510c44df67707109e38cc650
HeyStarrySky <107348714+HeyStarrySky@users.noreply.github.com>
2023-06-14 09:53:35 +0800

    打印奇偶数小工具

---
+++ src/data/structures/algorithm/test/PrintNum.java
@@ -0,0 +1,55 @@
+package data.structures.algorithm.test;
+
+import java.util.Arrays;
+
+/**
+ * Printing numbers pages for MI
+ * @author Administrator
+ */
+public class PrintNum {
+
+    /**
+     * 打印的纸张数
+     */
+    private static final int NUM_PAGES = 5;
+
+    /**
+     * 开始页码
+     */
+    private static final int START_PAGES = 1;
+
+    public static void main(String[] args) {
+        int[] array = new int[NUM_PAGES];
+        System.out.println("正面打印页码：" + Arrays.toString(new PrintNum().printOddNumbers(START_PAGES,array)));
+        System.out.println("反面打印页码：" + Arrays.toString(new PrintNum().printEvenNumbers(START_PAGES,array)));
+    }
+
+    /**
+     * 正面打印页码
+     * @param startNumber 开始页数
+     * @param containers 装载纸张数的数组容器
+     * @return 返回正面页码数组
+     */
+    private int[] printOddNumbers(int startNumber, int[] containers) {
+        for (int i = 0; i < NUM_PAGES; i++) {
+            containers[i] = startNumber;
+            startNumber = startNumber + 2;
+        }
+        return containers;
+    }
+
+    /**
+     * 反面打印页码
+     * @param startNumber 开始页数
+     * @param containers 装载纸张数的数组容器
+     * @return 返回反面页码数组
+     */
+    private int[] printEvenNumbers(int startNumber, int[] containers) {
+        int startEvenNumber = startNumber + 1;
+        for (int i = 0; i < NUM_PAGES; i++) {
+            containers[i] = startEvenNumber;
+            startEvenNumber = startEvenNumber + 2;
+        }
+        return containers;
+    }
+}
