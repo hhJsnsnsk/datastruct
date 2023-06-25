4fd9e9ea994841a46afd98c45894e030101be6ce
starrysky <miraculqueininity@gmail.com>
2023-06-26 00:47:06 +0800

    * 查询数组元素工具类  * Tools Class for querying array elements

--- src/data/structures/algorithm/datastructure/newstudy/array/ArrayTest.java
+++ src/data/structures/algorithm/datastructure/newstudy/array/ArrayTest.java
@@ -1,14 +1,19 @@
 package data.structures.algorithm.datastructure.newstudy.array;
 
 import data.structures.algorithm.datastructure.newstudy.array.utils.ArrayUtils;
+import data.structures.algorithm.datastructure.newstudy.array.utils.SearchUtils;
 
 /**
  * 数组测试类
  * @author starrysky
  */
 public class ArrayTest {
+
+    private static final int[] ARRAY_TEST = new int[]{99,99,98,97,96,95,94,93,92,91,90,0,0,1,1,2,2,33,44,55,6};
+
     public static void main(String[] args) {
         ArrayUtils utils = new ArrayUtils();
+        SearchUtils su = new SearchUtils();
         utils.addElement(10);
         utils.addElement(10);
         utils.addElement(10);
@@ -19,5 +24,7 @@ public class ArrayTest {
         utils.insertElement(3,88);
         utils.printArray();
         System.out.println(utils.getElement(4));
+        System.out.println("======================");
+        System.out.println(su.searchElement(99, ARRAY_TEST));
     }
 }
---
+++ src/data/structures/algorithm/datastructure/newstudy/array/utils/SearchUtils.java
@@ -0,0 +1,29 @@
+package data.structures.algorithm.datastructure.newstudy.array.utils;
+
+/**
+ * 查询数组元素工具类
+ * Tools Class for querying array elements
+ * @author starrysky
+ */
+public class SearchUtils {
+
+    /**
+     * 查询某一个数是否在数组里，存在则返回对应的索引
+     * Query whether a certain number is in the array,if it exists,return the corresponding index
+     * @param number 需要查询的数据 The data to be queried
+     * @return 返回索引值 return index value
+     */
+    public int searchElement(int number,int[] array) {
+        // 对索引赋值，-1 为不存在
+        int index = -1;
+        // 遍历原数组，查询是否存在指定数据data，存在则将index赋值为指定的索引值
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == number) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+}
