87db82281b29157f010e1056fc1583dba3ac1e83
starrysky <miraculqueininity@gmail.com>
2024-02-27 16:25:01 +0800

    20 有效的括号 消除法

--- src/data/lession/firstChapter/FirstChapter.java
+++ src/data/lession/firstChapter/FirstChapter.java
@@ -3,7 +3,6 @@ package lession.firstChapter;
 import java.util.Stack;
 
 /**
- *
  * @author starrysky
  */
 public class FirstChapter {
@@ -20,13 +19,13 @@ public class FirstChapter {
      * @param s input string
      * @return 栈元素是否为空
      */
-    boolean isValid(String s){
+    boolean isValid(String s) {
         // 当字符串为空时，返回true
-        if (s.isEmpty()){
+        if (s.isEmpty()) {
             return true;
         }
         // 当字符串为奇数个，返回false
-        if (s.length() % 2 == 1){
+        if (s.length() % 2 == 1) {
             return false;
         }
         // 消除法的主要核心逻辑
@@ -34,11 +33,11 @@ public class FirstChapter {
         for (int i = 0; i < s.length(); i++) {
             // 取出字符串
             char c = s.charAt(i);
-            if (c == '(' ){
+            if (c == '(') {
                 // 压栈
                 t.push(c);
-            }else if (c==')'){
-                if (t.empty()){
+            } else if (c == ')') {
+                if (t.empty()) {
                     // 在首次出现‘）’，返回false
                     return false;
                 }
@@ -50,11 +49,63 @@ public class FirstChapter {
         return t.empty();
     }
 
+    /**
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     * An input string is valid if:
+     * Open brackets must be closed by the same type of brackets.
+     * Open brackets must be closed in the correct order.
+     * Every close bracket has a corresponding open bracket of the same type.
+     *
+     * @param s
+     * @return 栈元素是否为空
+     */
+    boolean isValid2(String s) {
+        if (s == null || s.isEmpty()) {
+            // 如果字符串为空或字符串的长度为0则返回true
+            return true;
+        }
+        if (s.length() % 2 == 1) {
+            // 如果字符串个数为奇数个，返回false
+            return false;
+        }
+        // 定义栈，接受字符串类型
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            // for循环取出字符
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                // 输入为左括号则压栈
+                stack.push(c);
+            } else if (c == ')') {
+                // 输入为‘）’则验证前面是否出现‘（’
+                if (stack.isEmpty() || stack.peek() != '(') {
+                    return false;
+                }
+                // 符合上述条件则弹栈，栈元素为0
+                stack.pop();
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.peek() != '{') {
+                    return false;
+                }
+                stack.pop();
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
+                    return false;
+                }
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+            return stack.empty();
+        }
 
 
 
     public static void main(String[] args) {
         FirstChapter q = new FirstChapter();
         System.out.println(q.isValid("(()()(()))"));
+        System.out.println(q.isValid2("()[]{}"));
+        System.out.println(q.isValid2("(]"));
     }
 }
