ec2609596ed48b487c127539491755be7c7ed3cc
starrysky <miraculqueininity@gmail.com>
2024-07-19 01:14:42 +0800

    使用链表实现栈

--- src/cartoonalgo/datastruct/LinkedNodeClass.java
+++ src/cartoonalgo/datastruct/LinkedNodeClass.java
@@ -1,5 +1,7 @@
 package cartoonalgo.datastruct;
 
+import java.util.Arrays;
+
 /**
  * @author starrysky
  */
@@ -22,39 +24,97 @@ public class LinkedNodeClass {
     private LinkNode stkPeek;
     private int stkSize = 0;
 
-    public LinkedNodeClass(){
+    public LinkedNodeClass() {
         stkPeek = null;
     }
 
     /**
      * 获取栈长度
+     *
      * @return 返回栈长度
      */
-    public int size(){
+    public int size() {
         return stkSize;
     }
 
     /**
      * 判断栈是否为空
+     *
      * @return t o f
      */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         return size() == 0;
     }
 
     /**
      * 入栈操作
+     *
      * @param num 入栈元素
      */
-    public void push(int num){
+    public void push(int num) {
         // 进栈的新节点 成为栈顶元素
         LinkNode node = new LinkNode(num);
         // 新栈顶的下一个节点为原栈顶
-        node.next= stkPeek;
+        node.next = stkPeek;
         // 重新赋值新栈顶
         stkPeek = node;
         // 元素个数 +1
-        stkSize ++;
+        stkSize++;
+    }
+
+    /**
+     * 获取栈顶元素
+     *
+     * @return 返回栈顶
+     */
+    public int peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        return stkPeek.val;
+    }
+
+    /**
+     * 出栈操作
+     * @return 返回出栈值
+     */
+    public int pop() {
+        int po = peek();
+        stkPeek = stkPeek.next;
+        stkSize--;
+        return po;
+    }
+
+    /**
+     * 将List转为Array并返回
+     * @return 返回array
+     */
+    public int[] toArray(){
+        LinkNode node = stkPeek;
+        int[] array = new int[size()];
+        for (int i = array.length-1; i >= 0; i--) {
+            array[i] = node.val;
+            node = node.next;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        LinkedNodeClass nodeClass = new LinkedNodeClass();
+        nodeClass.push(1);
+        nodeClass.push(2);
+        nodeClass.push(3);
+        nodeClass.push(4);
+        nodeClass.push(5);
+        System.out.println(Arrays.toString(nodeClass.toArray()));
+
+        // 访问栈顶
+        int top = nodeClass.peek();
+        System.out.println(top);
+
+        //
+        System.out.println(nodeClass.size());
+        System.out.println(nodeClass.isEmpty());
     }
 
 }
