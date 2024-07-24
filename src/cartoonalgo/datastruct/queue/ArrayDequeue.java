ba50de25d1797dd57f758becd59530ead4b659b9
starrysky <miraculqueininity@gmail.com>
2024-07-24 21:58:18 +0800

    构造函数初始化

--- src/cartoonalgo/datastruct/queue/ArrayDequeue.java
+++ src/cartoonalgo/datastruct/queue/ArrayDequeue.java
@@ -2,6 +2,7 @@ package cartoonalgo.datastruct.queue;
 
 /**
  * 数组实现双向队列
+ *
  * @author starrysky
  */
 public class ArrayDequeue {
@@ -12,5 +13,9 @@ public class ArrayDequeue {
     // 队首指针索引
     private int front;
 
+    public ArrayDequeue(int capacity) {
+        queue = new int[capacity];
+        queSize = front = 0;
+    }
 
 }
